bb29277c704ef1993e8c40332fc2cd203555bfef
Danielle Veillet <nomi850@hotmail.com>
2018-05-02 17:57:16 -0400

    Essai pour utilisation de ignore
    Essai test

---
+++ GoGreen/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float movespeed = 2f;  // Use this for initialization
+    // Rigidbody thisRigidbody;
+    private Vector3 input;
+    void Start () {
+        print("Starting");
+        //thisRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		input = new Vector3(Input.GetAxis("Horizontal")*Time.deltaTime, 0.0f, Input.GetAxis("Vertical")*Time.deltaTime);
+        //print(input);
+	//	thisRigidbody.AddForce (input * movespeed); // Ne fonctionne pas pour l'instant...
+
+		transform.Translate(movespeed * input);
+    }
+}
